fc563a3b9db206cc2ba23230c6ceead3e50b99b8
Seong il Lee <qhtmfqhtmf2@gmail.com>
2023-01-20 21:53:22 +0900

    day15

--- BasicJava/src/chapter15/Car.java
+++ BasicJava/src/chapter15/Car.java
@@ -24,9 +24,13 @@ public class Car {
 	public void setColor(String color) {
 		this.color = color;
 	}
-	//public boolean speedUp(double speed) {
-		
-//	}
+	public boolean speedUp(double speed) {
+		if(double speed >200) {
+			System.out.println("속도 변경 불가능");
+		}else {
+			System.out.println("속도 변경 가능");
+		}return;
+	}
 	public static double getMaxSpeed() {
 		return MAX_SPEED;
 	}
--- BasicJava/src/chapter15/CarTest.java
+++ BasicJava/src/chapter15/CarTest.java
@@ -6,6 +6,9 @@ public class CarTest {
 		Car mycar = new Car("red");
 		System.out.println("color: " + mycar.getColor());
 		System.out.println("maxSpeed"+Car.getMaxSpeed()+"km/h");
+		
+		System.out.println("첫번째 speedUp(100.0km/h): ");
+		if(mycar.s)
 
 	}
 
