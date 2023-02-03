05993b8da0323716219690521808239c899d2f27
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2023-02-03 14:14:55 +0900

    Update BankAccount.java

--- class02/BankAccount.java
+++ class02/BankAccount.java
@@ -23,16 +23,16 @@ public abstract class BankAccount {
 	    }
 	    
 
-	    public boolean transfer(int amount,BankAccount otherAccount) throws IllegalArgumentException{ 
-
+	    public boolean transfer(int amount,BankAccount otherAccount) //throws IllegalArgumentException : 굳이 할 필요없음
+	    { 
 	       if(amount < 0||amount > balance){   
 	          throw new IllegalArgumentException();
+	       }if(otherAccount == null){
+		  throw new NullPointException();
+	       }if(withdraw(amount){
+		   otherAccount.deposit(amount);
+		   return true;
 	       }
-
-	       this.balance -= amount;
-	      otherAccount.deposit(amount);
-	      return true;
-
 	    }
 
 	    public String toString() {
