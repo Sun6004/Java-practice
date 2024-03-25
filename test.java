32c7f828ad7a8a8c5fa5a21bd44bc918b0fd8c68
Seong il Lee <118361080+Sun6004@users.noreply.github.com>
2024-03-25 16:16:10 +0900

    정처기 기출문제 java
    .

---
+++ test.java
@@ -0,0 +1,21 @@
+class Static{
+    public int a = 20;
+    static int b = 0;
+}
+ 
+public class Main{
+    public static void main(String[] args) {
+        
+        int a;
+        a = 10;
+        Static.b = a;
+ 
+        Static st = new Static();
+ 
+        System.out.println(Static.b++); 
+        System.out.println(st.b);
+        System.out.println(a);
+        System.out.print(st.a);
+    }
+}
+// 결과 10 11 10 20
